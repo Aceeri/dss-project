4a65055394dd68f0041c73f9aea4cccea29b3948
Aceeri <conmcclusk@gmail.com>
2021-10-07 17:34:25 -0700

    Add alpha blending

--- src/renderer/renderer.rs
+++ src/renderer/renderer.rs
@@ -167,7 +167,7 @@ impl Renderer {
                 entry_point: "main",
                 targets: &[wgpu::ColorTargetState {
                     format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
+                    blend: Some(wgpu::BlendState::ALPHA_BLENDING),
                     write_mask: wgpu::ColorWrites::ALL,
                 }],
             }),
